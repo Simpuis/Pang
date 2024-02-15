91bf168a9a059dd35e61e3700f0a254a7ebd3910
Freja <freyjaedgr@gmail.com>
2024-02-15 21:00:09 +0100

    refactoring extension serializer

--- src/serialization/serializers/extension_serializer.h
+++ src/serialization/serializers/extension_serializer.h
@@ -17,24 +17,32 @@ class extension_serializer : public serializer {
     void save_types(serializer_save_data& save_data) {
         constexpr auto type = refl::reflect<Head_T>();
         save_data.world.each<Head_T>([&](flecs::entity entity, Head_T& component) {
+            tinygltf::Value::Object object = serialize_component(save_data, component);
+
             tinygltf::Node& gltf_node = save_data.model.nodes[save_data.entity_node_map[entity]];
-
-            std::map<std::string, tinygltf::Value> object;
-            for_each(type.members, [&](auto member) {
-                if constexpr(is_readable(member) && refl::descriptor::has_attribute<serializable>(member)) {
-                    std::pair<std::string, tinygltf::Value> pair;
-                    pair.first = get_display_name(member);
-                    assign_component(pair.second, member(component), save_data);
-                    object.insert(pair);
-                }
-            });
-
             gltf_node.extensions[type.name.c_str()] = tinygltf::Value(object);
         });
 
         if constexpr(sizeof...(Tail_Ts) > 0) {
             save_types<Tail_Ts...>(save_data);
         }
+    }
+
+    template<typename Head_T>
+    tinygltf::Value::Object serialize_component(serializer_save_data &save_data, const Head_T &component) const {
+        constexpr auto type = refl::reflect<Head_T>();
+        std::map<std::string, tinygltf::Value> object;
+
+        for_each(type.members, [&](auto member) {
+            if constexpr(is_readable(member) && refl::descriptor::has_attribute<serializable>(member)) {
+                std::pair<std::string, tinygltf::Value> pair;
+                pair.first = get_display_name(member);
+                assign_component(pair.second, member(component), save_data);
+                object.insert(pair);
+            }
+        });
+
+        return object;
     }
 
     template<typename Head_T, typename... Tail_Ts>
